f01b4a6c47aedaaa3d0c97db8eb41a0f73f8f4a8
Godefroid Chapelle <gotcha@bubblenet.be>
2024-10-21 11:13:20 +0200

    pyroma 10/10

--- setup.py
+++ setup.py
@@ -11,7 +11,20 @@ setup(
     long_description=open("README.rst").read()
     + "\n"
     + open("CHANGES.rst").read(),
-    classifiers=["Framework :: Plone", "Programming Language :: Python"],
+    classifiers=[
+       "Framework :: Plone",
+       "License :: OSI Approved :: GNU General Public License (GPL)",
+	   "Development Status :: 5 - Production/Stable",
+       "Programming Language :: Python",
+	   "Programming Language :: Python :: 2",
+	   "Programming Language :: Python :: 2.7",
+	   "Programming Language :: Python :: 3",
+	   "Programming Language :: Python :: 3.7",
+	   "Programming Language :: Python :: 3.8",
+	   "Programming Language :: Python :: 3.9",
+	   "Programming Language :: Python :: 3.10",
+	   "Programming Language :: Python :: 3.11",
+	   "Programming Language :: Python :: 3.12"],
     keywords='plone.app.theming diazo jbot views',
     author='Martin Aspeli',
     author_email='optilude@gmail.com',
@@ -21,6 +34,7 @@ setup(
     namespace_packages=['plone', 'plone.app'],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=2.7,!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*, <3.13",
     install_requires=[
         'plone.app.theming',
         'plone.resource',
